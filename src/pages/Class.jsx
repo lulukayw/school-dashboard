ae5cb27fe3679f829117b5740f57af56b03d5052
kuchimpos <kuchimpos@g.ucla.edu>
2026-05-18 15:00:38 -0700

    Create page routing
    Installed
    react-router-dom and set up routing in main.j
    sx and App.jsx. Created placeholder for each
    dashboard page in pages folder for testing pu
    rposes. App.jsx also has a temporary nav elem
    ent for testing purposes.

---
+++ src/pages/Class.jsx
@@ -0,0 +1,3 @@
+export default function Class() {
+  return <div>This is the class view page.</div>;
+}
---
+++ src/pages/Classes.jsx
@@ -0,0 +1,3 @@
+export default function Classes() {
+  return <div>This is the classes directory page.</div>;
+}
