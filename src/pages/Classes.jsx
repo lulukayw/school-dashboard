a2204e70ae281044daf1e70fbec230cdcb493174
kuchimpos <kuchimpos@g.ucla.edu>
2026-05-19 12:15:55 -0700

    Fetch classes on page load
    Define useEffect to load all classes on page load

--- src/pages/Classes.jsx
+++ src/pages/Classes.jsx
@@ -1,3 +1,31 @@
+import { useState, useEffect } from "react";
+import { fetchAllClasses, studentsFromClass } from "../util/ClassServices";
+
 export default function Classes() {
-  return <div>This is the classes directory page.</div>;
+  const [classes, setClasses] = useState([]);
+
+  useEffect(() => {
+    const loadClasses = async () => {
+      try {
+        const data = await fetchAllClasses();
+        setClasses(data);
+      } catch (e) {
+        console.error(
+          "Error fetching classes: ",
+          e.message ?? "No error message",
+        );
+      }
+    };
+    loadClasses();
+  }, []);
+
+  return (
+    <div>
+      {classes.map((c) => (
+        <p>
+          {c.average_grade} {c.name} {c.teacher_id}
+        </p>
+      ))}
+    </div>
+  );
 }
