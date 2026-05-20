e8b28940d85d680c22d6f0ec1dc818359ad57a19
kuchimpos <kuchimpos@g.ucla.edu>
2026-05-20 12:01:25 -0700

    Refactor CreateClassForm to use teachers array
    Fixed create class functionality by replacing teacherMap with a teachers array in CreateClassForm

--- src/pages/Classes.jsx
+++ src/pages/Classes.jsx
@@ -78,7 +78,7 @@ export default function Classes() {
         <CreateClassForm
           handleSubmit={handleSubmit}
           handleExit={handleExit}
-          teacherMap={teacherMap}
+          teachers={teachers}
         />
       )}
     </>
