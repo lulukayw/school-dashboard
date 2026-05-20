0bc9a4cff9195b8a53a2f36ce9ffe420c736c2cd
Ryan Taylor <rtaylor003376@gmail.com>
2026-05-20 08:55:10 -0400

    Pulling Lulu's changes

--- src/pages/Teachers.jsx
+++ src/pages/Teachers.jsx
@@ -1,3 +1,57 @@
+import { useState, useEffect } from "react";
+import { db } from "../../firebase";
+
+import "../App.css";
+import {
+  collection,
+  getDocs,
+  updateDoc,
+  doc,
+  increment,
+  addDoc,
+} from "firebase/firestore";
 export default function Teachers() {
-  return <div>This is the teachers directory page.</div>;
+  const [teachers, setTeachers] = useState([]);
+  const [newTeachers, setNewTeachers] = useState([]);
+  const fetchResponses = async () => {
+    const querySnapshot = await getDocs(collection(db, "teachers"));
+    console.log("fetched docs:", querySnapshot.docs.length);
+    const data = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    setTeachers(data);
+  };
+
+  useEffect(() => {
+    fetchResponses();
+  }, []);
+
+  const handleNewTeachers = async () => {
+    const docRef = await addDoc(collection(db, "teachers"), {
+      Teacher: newTeacher,
+    });
+    setNewResponse("");
+    fetchResponses();
+  };
+
+  const handleDeleteTeachers = async () => {
+    const docRef = await addDoc(collection(db, "teachers"), {
+      Teacher: 0,
+    });
+    setNewResponse("");
+    fetchResponses();
+  };
+  return (
+    <div className="Teachers">
+      <header className="Teachers-header">Teachers:</header>
+      <ul>
+        {teachers.map((teachers) => (
+          <li key={teachers.id}>
+            {teachers.first_name} {teachers.last_name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
